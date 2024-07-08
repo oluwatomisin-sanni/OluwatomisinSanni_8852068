0c6be047fd5380011e44f47ca225d841dd98caaf
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-08 18:39:08 -0400

    loading up item

--- Assignment3/OluwatomisinSanni_8852068_Assignment3.py
+++ Assignment3/OluwatomisinSanni_8852068_Assignment3.py
@@ -12,15 +12,20 @@ time.sleep(4)
 #locating sign up button on home page
 signin_button = driver.find_element("id","a2700.product_home_l0.home_header.i5.6b7667af7KbaEM")
 signin_button.click()
+time.sleep(4)
 
 #signing into customer account
 username_bar = driver.find_element("id","a2700.login.0.i0.56be1afaWMreeL")
 username_bar.send_keys("sannitomi@outlook.com")
 password_bar = driver.find_element("name","password")
 password_bar.send_keys("maybelater24")
-login_button=driver.find_element("id""a2700.login.0.i3.56be1afaspOapR")
+login_button=driver.find_element("id","a2700.login.0.i3.56be1afaspOapR")
 login_button.click()
+time.sleep(4)
 
+#search for item
+search_bar= driver.find_element("class","search-bar-input util-ellipsis")
+search_bar.send_keys("humidifier")
+search_bar.send_keys(Keys.RETURN)
+time.sleep(4)
 
-
-search_bar.send_keys("laptop")
