70b76846741f88b5cbb139e5639dcb8a3db0bd78
oluwatomisin-sanni <Osanni2068@conestogac.on.ca>
2024-07-09 13:57:36 -0400

    selecting the sandal category from homepage

--- Assignment3/OluwatomisinSanni_8852068_Assignment3.py
+++ Assignment3/OluwatomisinSanni_8852068_Assignment3.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
+
+#opening the site in chrome
+driver.get("https://www.zappos.com/")
+driver.maximize_window()
+time.sleep(6)
+
+#selecting the sandal category from the homepage
+sandal= driver.find_element("xpath","//*[@id='main']/div/div/section[1]/div/article[3]/a")
+sandal.click()
+time.sleep(6)
+assert "sandals" in driver.title
